a8ac77ac8e94b16103648b119b93748f4f69b43d
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-02-06 18:24:11 +0000

    Hero class added

--- js.js
+++ js.js
@@ -1,4 +1,15 @@
 //need a hero
+const class Hero={
+	.contructor(name){
+		this.name="name"
+		this.health=100
+		this.weapon={
+			name="Whip",
+			damage=5
+	}
+
+}
+
 //need an enemy class
 //need a monkey
 //need a final boss
@@ -6,3 +17,4 @@
 // spells items
 //inventory
 //log
+const pipo=document.getElementById("log").setAttribute("pipo")

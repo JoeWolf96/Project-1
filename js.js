00ac11e7a0ef62e666653a4b9f40100a22be911f
Gioele Rossi <gioelerossi@Gioeles-Mac-mini.local>
2021-02-10 16:53:35 +0000

    added buttons monster class and final boss class. working on movement. removed 3 rooms

--- js.js
+++ js.js
@@ -7,31 +7,107 @@
 			name:"whip",
 			damage:5
 		}
+	    this.location= $(".room room1")
 	}
 
 	announceHealth() {
 
-		console.log(this.health);
-
+	console.log(this.health)
+    
 	}
-
 	raiseWeapon() {
 
 		console.log(this.name+" raises his "+this.weapon.name);
+		
+    }
+     fight() {
+
+
+    
+    }
+    moveUp() {
+  //   	handleClick: function(e) {
+		// console.log(this)
+	 //         }
+          
+  //           const button = $(".button-up")
+  //           button.addEventListener("click", e => moveUp.handleClick(e))
+
+    }
+     announceLocation(){
+     	console.log(this.location)
+     }
+ }
+
+
+ //need a room class
+
+
+
+ //need an enemy class
+class Monster{
+	constructor(name,health,damage){
+		this.name=name ;
+		this.health=health ;
+		this.damage=damage 
+
+	}
+}
+const skeleton= new Monster("skeleton",10,1)
+console.log(skeleton)
+
+const zombie = new Monster("zombie",15,2)
+console.log(zombie)
+
+//inventory
+
+class Inventory{
+	constructor(){
+		this.healthPotion="Health Potion"
+		this.cross="Cross"
+	}
+	heal(){
 
 	}
 
 }
 
+
+//create hero
+
 const belmont = new Hero();
+
+
 console.log(belmont)
 belmont.announceHealth();
 belmont.raiseWeapon()
-//need an enemy class
-//need a monkey
+belmont.moveUp()
+window.onload = function(){
+        const inventoryBox = document.getElementById("inventory");
+        const inventory= new Inventory()
+       
+
+       inventoryBox.value=inventory
+          }
+
+
 //need a final boss
-// disappearing hud
-// spells items
-//inventory
+class FinalBoss{
+	constructor(){
+		this.health=100
+		this.damage=10
+	}
+
+
+}
+
+
 //log
+
 //movement
+
+
+
+
+
+

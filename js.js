57f3890024a8d066fa0fc2fff83f07066e284f16
JoeWolf96 <75704432+JoeWolf96@users.noreply.github.com>
2021-02-13 13:29:02 +0000

    Update js.js

--- js.js
+++ js.js
@@ -31,6 +31,8 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
 
     }
      fightBegins(Monster , room) {
+        //prevent belmont from escape
+        $('#hero-image').attr('draggable', false);
 
         let snd = new Audio("walk.wav")
          snd.play()
@@ -39,7 +41,7 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
         const eventLog=$(".log")
 
         eventLog.append(message)
-        console.log(eventLog.html())
+        
 
     }
     attack(Monster){ 
@@ -55,7 +57,6 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
 
         let  dmg = this.weapon.damage ;
         Monster.health -= dmg
-     
 
         const enemyHp="--"+Monster.name+" has "+Monster.health+"HP remaining"
         const eventLog2=$(".log");
@@ -66,12 +67,14 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
        //enemy attacks
 
         this.health -= Monster.damage
+        console.log(this.health)
+
         if(this.health <=0){
             alert("You Lost, refresh the page to try again")
             $(".fight-buttons").off("click");
          $('#hero-image').attr('draggable', false);
-         $("#skeleton").attr("draggable",false)
         }
+
 
        // update hp 
        
@@ -89,8 +92,9 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
         const eventLog=$(".log");
         eventLog.append(message);
 
-       //end of the fight ui disappears
-        $("#skeleton").css("visibility","hidden")
+       //end of the fight ui disappears and you can move again
+       $('#hero-image').attr('draggable', true);
+       $("#skeleton").css("visibility","hidden")
         
         
         $(".fight-buttons").css("visibility","hidden")
@@ -113,13 +117,17 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
         const eventLog2=$(".log");
         eventLog2.append(enemyHp)
        //enemy attacks
+
         this.health -= Boss.damage
+
         if(this.health <=0){
             alert("You Lost, refresh the page to try again")
             $(".fight-buttons").off("click");
          $('#hero-image').attr('draggable', false);
-         $("#dracula").attr("draggable",false)
         }
+
+        
+
 
 
        // update hp 
@@ -170,8 +178,9 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
             alert("You Lost, refresh the page to try again")
             $(".fight-buttons").off("click");
          $('#hero-image').attr('draggable', false);
-         $("#slogra").attr("draggable",false)
+         $("#dracula").attr("draggable",false)
         }
+
          
  
 
@@ -263,15 +272,18 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
 
         let  dmg = 80 ;
         Boss.health -= dmg
-        console.log(Boss.health)
 
+        const enemyHp=Boss.name+" has "+Boss.health+"HP remaining"
+        const eventLog2=$(".log");
+        eventLog2.append(enemyHp)
+       
          this.inventory.holy_waters -= 1
  
         if(this.inventory.holy_waters== 0){
             const holyButton = $("#holywater")
             holyButton.remove()}
 
-             else if (Boss.health == 0){
+        if (Boss.health <= 0){
             $("#dracula").css("visibility","hidden")
             $(".holy-water").css("visibility","hidden")
             $(".fight-buttons").css("visibility","hidden")
@@ -293,6 +305,9 @@ return myAudio.paused ? myAudio.play() : myAudio.pause();
 //create hero
 
 const belmont = new Hero();
+
+
+
 
 
 console.log(belmont)
@@ -332,7 +347,7 @@ class Boss{
 const dracula = new Monster("Dracula",100,15)
 
 
-//key 
+
 
 
 
@@ -416,7 +431,7 @@ function onDrop4(event){
 
 function onDrop5(event){
      event.preventDefault()
-     belmont.fightBegins()
+     
   
     let position = $("#room5")
     position.append($("#hero-image"))
@@ -441,13 +456,13 @@ function onDrop6(event){
 
 
 function onDrop7(event){
-   
+    
     
 
 
      event.preventDefault()
      if(belmont.key == true){
-         $("#myAudio").remove()
+        $("#myAudio").remove()
     let snd = new Audio("final_boss.mp3")
          snd.play()
   
